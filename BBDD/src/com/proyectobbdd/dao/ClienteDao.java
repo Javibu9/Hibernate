8c4af4248dcaf499cd47f38cd77412695cf7a114
SoftwareIFP <52700609+SoftwareIFP@users.noreply.github.com>
2020-02-25 09:16:42 +0100

    rama javi

--- BBDD/src/com/proyectobbdd/dao/ClienteDao.java
+++ BBDD/src/com/proyectobbdd/dao/ClienteDao.java
@@ -6,6 +6,7 @@ import com.proyectobbdd.domain.Factura;
 
 public interface ClienteDao {
 
+	//hola
 	public void save (Cliente cliente);
 	public List<Factura> findall(Cliente cliente);
 	public Cliente findById(int idCliente);
